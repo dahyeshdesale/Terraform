ea6b02ea37a7fdb89278bd39a9f1e545b5e8ec4d
dahyeshdesale <dreamdesale108@gmail.com>
2024-11-29 15:38:30 +0530

    update

--- IAM.tf
+++ IAM.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "random_password" "random_pas" {
+  length = 12
+  upper = true
+  lower = true
+  number = true
+  special = true
+}
+
 # Create IAM User
 resource "aws_iam_user" "dreamdesale" {
   name = "dreamdesale"
@@ -11,7 +19,7 @@ resource "aws_iam_user" "dreamdesale" {
 # Console Login
 resource "aws_iam_user_login_profile" "terraformlogin" {
   user = aws_iam_user.dreamdesale.name
-  password = "Dahyesh@111999"
+  password = random_password.random_pas.result
   password_reset_required = false
 }
 
