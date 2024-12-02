b07d61aa98d91e46b4661dfb6b968dbfa95729a2
dahyeshdesale <dreamdesale108@gmail.com>
2024-12-02 16:35:33 +0530

    new id

---
+++ New Project/Iam.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_iam_account_password_policy" "dahyesh" {
+  minimum_password_length        = 8
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+}
+
+resource "aws_iam_user" "dahyesh_user" {
+  name = "dahyesh"
+  force_destroy = true
+}
+
+resource "aws_iam_user_login_profile" "dahyesh_login" {
+  user = aws_iam_user.dahyesh_user.name
+  password_reset_required = true
+}
