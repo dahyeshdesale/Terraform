4c207ba83a50743bf568bf64319b11846f9adf9f
dahyeshdesale <dreamdesale108@gmail.com>
2024-12-02 18:02:19 +0530

    new password newsv13

--- New Project/Iam.tf
+++ New Project/Iam.tf
@@ -39,7 +39,7 @@ resource "local_file" "info_user_login" {
   username: ${aws_iam_user.dahyesh_user.name}
   password: ${aws_iam_user_login_profile.dahyesh_login.password}
   console_signin_url: https://${data.aws_caller_identity.current.account_id}.signin.aws.amazon.com/console
-    access key id: ${aws_iam_access_key.dahyesh_user.id}
+  access key id: ${aws_iam_access_key.dahyesh_user.id}
   secret access key: ${aws_iam_access_key.dahyesh_user.secret}
   EOF
   filename = "${path.module}/info_user_login.txt"
