ec41c6be31a986f2bad5bab2e4ee8c12ac27b48f
dahyeshdesale <dreamdesale108@gmail.com>
2024-12-09 14:11:53 +0530

    new projectv4

--- New Project/S3 data transfer code/s3.tf
+++ New Project/S3 data transfer code/s3.tf
@@ -8,40 +8,23 @@ resource "aws_s3_bucket" "source_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_versioning" "versioning_configuration_source" {
+  bucket = aws_s3_bucket.source_bucket.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket" "destination_bucket" {
   bucket = "dsahyeshdesaledatadestination"
   acl = "public-read"
   force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "source_bucket_policy" {
-  bucket = aws_s3_bucket.source_bucket.id
-  policy = <<POLICY
-{ "Version": "2012-10-17", 
-  "Statement": [ 
-  { "Effect": "Allow", 
-    "Principal": "*", 
-    "Action": "s3:GetObject", 
-    "Resource": "${aws_s3_bucket.source_bucket.arn}/*" 
-  } 
-] 
-} 
-POLICY  
+resource "aws_s3_bucket_versioning" "versioning_configuration_destination" {
+  bucket = aws_s3_bucket.destination_bucket.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
-
-resource "aws_s3_bucket_policy" "destination_bucket_policy" {
-  bucket = aws_s3_bucket.destination_bucket.id
-  policy = <<POLICY
-{ "Version": "2012-10-17", 
-  "Statement": [ 
-  { "Effect": "Allow", 
-    "Principal": "*", 
-    "Action": "s3:GetObject", 
-    "Resource": "${aws_s3_bucket.destination_bucket.arn}/*" 
-  } 
-] 
-} 
-POLICY  
-}
-
 
