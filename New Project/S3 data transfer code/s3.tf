1164f66301c10d3fb7d2207cda83f9e81cb2b38c
dahyeshdesale <dreamdesale108@gmail.com>
2024-12-09 13:31:39 +0530

    new projectv1

--- New Project/S3 data transfer code/s3.tf
+++ New Project/S3 data transfer code/s3.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "publicbucket" {
-  bucket = "dahyeshdesaledataspace"
+resource "aws_s3_bucket" "source_bucket" {
+  bucket = "dahyeshdesaledatasource"
   acl = "public-read"
   versioning {
    enabled = true 
@@ -11,5 +11,43 @@ resource "aws_s3_bucket" "publicbucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket" "destination_bucket" {
+  bucket = "dsahyeshdesaledatadestination"
+  acl = "public-read"
+  versioning {
+    enabled = true
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "source_bucket_policy" {
+  bucket = aws_s3_bucket.source_bucket.id
+  policy = <<POLICY
+{ "Version": "2012-10-17", 
+  "Statement": [ 
+  { "Effect": "Allow", 
+    "Principal": "*", 
+    "Action": "s3:GetObject", 
+    "Resource": "${aws_s3_bucket.source_bucket.arn}/*" 
+  } 
+] 
+} 
+POLICY  
+}
+
+resource "aws_s3_bucket_policy" "destination_bucket_policy" {
+  bucket = aws_s3_bucket.destination_bucket.id
+  policy = <<POLICY
+{ "Version": "2012-10-17", 
+  "Statement": [ 
+  { "Effect": "Allow", 
+    "Principal": "*", 
+    "Action": "s3:GetObject", 
+    "Resource": "${aws_s3_bucket.destination_bucket.arn}/*" 
+  } 
+] 
+} 
+POLICY  
+}
 
 
