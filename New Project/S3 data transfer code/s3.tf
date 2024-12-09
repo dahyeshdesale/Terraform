b55a85b88f8ec433d257ff5132cbde0f8919f6da
dahyeshdesale <dreamdesale108@gmail.com>
2024-12-09 14:30:49 +0530

    new projectv9

--- New Project/S3 data transfer code/s3.tf
+++ New Project/S3 data transfer code/s3.tf
@@ -9,7 +9,8 @@ resource "aws_s3_bucket" "source_bucket" {
 
 resource "aws_s3_bucket_acl" "source_acl" {
   bucket = aws_s3_bucket.source_bucket.bucket
-  acl = "private"
+  acl = private
+  
 }
 
 resource "aws_s3_bucket_versioning" "versioning_configuration_source" {
@@ -26,7 +27,7 @@ resource "aws_s3_bucket" "destination_bucket" {
 
 resource "aws_s3_bucket_acl" "destination_acl" {
   bucket = aws_s3_bucket.destination_bucket.bucket
-  acl = "private"
+  acl = private
 }
 
 resource "aws_s3_bucket_versioning" "versioning_configuration_destination" {
