59c69c7d88737408bc06123a5fb78a592b2a2c3c
dahyeshdesale <dreamdesale108@gmail.com>
2024-12-10 14:14:36 +0530

    new code

--- New Project/S3 data transfer code/s3.tf
+++ New Project/S3 data transfer code/s3.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "source_bucket" {
-  bucket = "dahyeshdesaledatasource"
+  bucket = var.bucket
   force_destroy = true
 }
 
@@ -12,6 +12,32 @@ resource "aws_s3_bucket_versioning" "versioning_configuration_source" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "source_bucket" {
+  bucket = aws_s3_bucket.source_bucket.id
+  rule {
+    object_ownership = "BucketOwerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "source_bucket" {
+  bucket = aws_s3_bucket.source_bucket.id
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "source_bucket" {
+  depends_on = [ 
+  aws_s3_bucket_ownership_controls.source_bucket,
+  aws_s3_bucket_public_access_block.source_bucket ]
+
+  bucket = aws_s3_bucket.source_bucket.id
+  acl = "public-read"
+
 }
 
 resource "aws_s3_bucket" "destination_bucket" {
@@ -24,4 +50,29 @@ resource "aws_s3_bucket_versioning" "versioning_configuration_destination" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "destination_bucket" {
+  bucket = aws_s3_bucket.destination_bucket.id
+  rule {
+    object_ownership = "BucketOwerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "destination_bucket" {
+  bucket = aws_s3_bucket.destination_bucket.id
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "destination_bucket" {
+  depends_on = [ aws_s3_bucket_ownership_controls.destination_bucket, 
+  aws_s3_bucket_public_access_block.destination_bucket
+   ]
+
+   bucket = aws_s3_bucket.destination_bucket.id
+   acl = "public-read"
 }
